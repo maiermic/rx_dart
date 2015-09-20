5ea9d306013ddad9aa5456a0b012e6a5d10e9ee9
Michael Maier <maier1michael@gmail.com>
2015-09-20 06:47:27 +0200

    add delay test

---
+++ test/extensions/delay_test.dart
@@ -0,0 +1,40 @@
+// Copyright (c) 2015, Michael Maier. All rights reserved. Use of this source code
+// is governed by a MIT-style license that can be found in the LICENSE file.
+
+import 'package:rx_dart/rx_dart.dart';
+import 'package:rx_dart/rx_stream_creators.dart';
+import 'package:stream_test_scheduler/stream_test_scheduler.dart';
+import 'package:test/test.dart';
+
+
+void main() {
+  group('delay', () {
+    test('on empty stream completes empty', () {
+      var stream = new RxStream.empty().delay(new Duration(milliseconds: 30));
+      expect(stream.toList(), completion(equals([])));
+    });
+
+    test('preserves relative time interval', () async {
+      final scheduler = new TestScheduler();
+
+      final source = scheduler.createStream([
+        onNext(30, 1),
+        onNext(60, 2),
+        onNext(90, 3),
+        onCompleted(100)
+      ]);
+
+      final result = await scheduler.startWithCreate(() => rx(source).delay(new Duration(milliseconds: 30)));
+
+      expect(
+          result,
+          equalsRecords([
+            onNext(60, 1),
+            onNext(90, 2),
+            onNext(120, 3),
+            onCompleted(120)
+          ], maxDeviation: 20)
+      );
+    });
+  });
+}
